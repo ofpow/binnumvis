fb5844904cb0a4472794df94ac34520801377b61
Oliver Powell <oliver@drpowell.org>
2025-07-27 10:17:11 -0400

    64 bit visualizer

--- binnumvis.c
+++ binnumvis.c
@@ -6,22 +6,70 @@
 
 #include <raylib.h>
 
+#define define_array(_name, _type) \
+    typedef struct _name {         \
+        _type *data;               \
+        int64_t index;             \
+        int64_t capacity;          \
+    } _name                        \
+
+#include "buttons.h"
+
+uint64_t num = 0;
+
+bool raylib_color_equals(Color c1, Color c2) {
+    return (c1.r == c2.r && c1.g == c2.g && c1.b == c2.b && c1.a == c2.a);
+}
+
 int main(int argc, char **argv) {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(GetScreenWidth(), GetScreenHeight(), "BINNUMVIS");
     SetExitKey(KEY_Q);
 
+    Buttons buttons = (Buttons){
+        calloc(64, sizeof(Button)),
+        63,
+        64
+    };
+
+    for (int i = 0; i < 64; i++) {
+        buttons.data[i] = (Button){
+            { 16 + i * (GetScreenWidth() / 64), 200, GetScreenWidth() / 100, GetScreenHeight() / 38 },
+            "0",
+            RED,
+            i
+        };
+    }
+
+    char num_text[24];
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(DARKGRAY);
         DrawFPS(10, 10);
+        
+        sprintf(num_text, "%llu", num);
+
+        DrawText(num_text, 100, 100, 72, WHITE);
 
         for (int i = 0; i < 64; i++) {
-            DrawText("0", 16 + i * (GetScreenWidth() / 64), 200, GetScreenWidth() / 44, PINK);
-        }
+            Button b = buttons.data[i];
+            DrawRectangleRounded(b.rec, 0.5, 3, b.color);
+            
+            DrawText(b.text, b.rec.x + 2, b.rec.y, GetScreenWidth() / 56, WHITE);
 
+            if (CheckCollisionPointRec(GetMousePosition(), b.rec) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                uint64_t mask = (uint64_t)1 << i;
+                if (raylib_color_equals(b.color, RED)) {
+                    num |= mask;
+                    buttons.data[i].color = GREEN;
+                } else {
+                    num &= ~mask;
+                    buttons.data[i].color = RED;
+                }
+            }
+        }
 
         EndDrawing();
     }
